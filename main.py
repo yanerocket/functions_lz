4af8a385114f4e1d269082de2ad02a94c6481bbe
yanerocket <rocketinterstellar@tutamail.com>
2025-11-27 15:48:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import circle
+import temperature
+import is_even
+import time
+
+
+def main():
+    circle.circ()
+    temperature.temp()
+    is_even.func()
+    time.prim()
+
+if __name__ == '__main__':
+    main()
+
